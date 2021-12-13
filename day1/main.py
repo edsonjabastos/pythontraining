40c72a78e00c5e870c0db9fc4d852c83e1a6af1e
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-13 20:50:22 -0300

    Day 1 - printing

---
+++ day1/main.py
@@ -0,0 +1,6 @@
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print("print('what to print')")
+print('Hello world!\nHello world!\nHello world!')
+print('Hello' + ' ' + 'Edinho')
+    # print('oin moço >_<') ERRADO: o python não aceita que uma linha de código começe com espaços 
