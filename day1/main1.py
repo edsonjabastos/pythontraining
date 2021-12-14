a827dc94663a5eb111df4f5bd62c71699ea515e3
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-13 21:10:44 -0300

    Day 1 - inputs

---
+++ day1/main1.py
@@ -0,0 +1,4 @@
+# input() vai pegar a entrada do usuário no console
+# depois o print() vai escrever no console a palavra "Hello" e a entrada do usuário
+
+print('Hello' + input('What is your name?'))
