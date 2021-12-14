c386b4f7a06756d3d5240d2de14e405f8e2ddc65
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 20:38:11 -0300

    Day2 - tipos de dados

--- day1/main4.py
+++ day1/main4.py
@@ -1,9 +1,9 @@
 #1. Create a greeting for your program.
 print("'Hi people, let's go try create a band name!")
 #2. Ask the user for the city that they grew up in.
-city = input('Which city you grew up? ')
+city = input('Which city you grew up?\n')
 #3. Ask the user for the name of a pet.
-petName = input('What the name of your favorite pet? ')
+petName = input('What the name of your favorite pet?\n')
 #4. Combine the name of their city and pet and show them their band name.
 print('Your Band Name could be: ' + city + ' ' + petName)
 #5. Make sure the input cursor shows on a new line, see the example at:
---
+++ day2/main.py
@@ -0,0 +1,3 @@
+print("Hello"[0])
+print("123" + "456")
+print(123_456)
