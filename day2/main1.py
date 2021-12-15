74b608f139cf80b63e052d65ef60aa7de1792dd1
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 21:55:23 -0300

    Day2 - exercício 1

---
+++ day2/main1.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+first_digit = int(two_digit_number[0])
+
+second_digit = int(two_digit_number[1])
+
+# print(two_digit_number[0], first_digit, two_digit_number[1], second_digit)
+
+# print(type(two_digit_number[0]), type(first_digit), type(two_digit_number[1]), type(second_digit))
+
+# new_two_digit_number = int(two_digit_number)
+
+# print(two_digit_number[0] + two_digit_number[1])
+
+print(first_digit + second_digit)
