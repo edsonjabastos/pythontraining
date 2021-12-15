6ba6ea7b928b550b6d16b0404c235fb7e64afab6
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 21:36:54 -0300

    Day2 - erros de tipo, conversão de tipos e checando tipos

---
+++ day2/main0.py
@@ -0,0 +1,19 @@
+num_char = len(input("what is your name? "))
+
+new_num_char = str(num_char)
+
+print("You name has " + new_num_char + " characters.")
+
+# print(type(num_char))
+
+a = float(123)
+print(a, type(a))
+
+a = str(123123)
+print(a, type(a))
+
+a = int("123123")
+print(a, type(a))
+
+a = True
+print(a, type(a))
