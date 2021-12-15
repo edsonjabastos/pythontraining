cfd789321e51e6c45ea48a3fef1670f80a5a06a2
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-12-14 22:24:23 -0300

    Day2 - desafio imc

---
+++ day2/main3.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: \n"))
+weight = float(input("enter your weight in kg: \n"))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+BMI = int(weight / height ** 2)
+
+print(BMI)
