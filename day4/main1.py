7dc4f9b41f3d1561c29b8e91edaeca29b285dd68
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-03-25 12:46:45 -0300

    Day4 - introducing lists data structure

---
+++ day4/main1.py
@@ -0,0 +1,9 @@
+estados_do_brasil = ["Minas Gerais", "Goiás", "Rio Grande do Sul", "Rio Grande do Norte", "Roraima", "Rio de Janeiro", "Mato Grosso do Sul", "Amazônia", "Espírito Santo", "Bahia", "Pernambuco", "Maranhão", "Acre", "Sergipe", "Paraná"]
+
+estados_do_brasil[-2] = "Goiaba"
+
+estados_do_brasil.append("Tocantinho")
+
+print(estados_do_brasil)
+
+# print(estados_do_brasil[0], estados_do_brasil[-1], estados_do_brasil[-2])
