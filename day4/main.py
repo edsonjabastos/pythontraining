270d46b26684bfefad662147a485c7e4aad485ae
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-03-25 10:12:01 -0300

    Day4 - randomization numbers

---
+++ day4/main.py
@@ -0,0 +1,19 @@
+import random
+# import my_module
+
+random_integer = random.randint(0, 5)
+
+random_float = random.random()
+
+random_float_zero_to_five = (random.random()) + (random.randint(0, 4))
+random_float_zero_to_five2 = random.random() * 5
+
+print(random_integer)
+
+print(random_float)
+
+print(random_float_zero_to_five)
+
+print(random_float_zero_to_five2)
+
+# print(my_module.pi)
