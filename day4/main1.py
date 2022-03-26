8e0d26c635e8fa20361f68d5e526a7ce37e92b2d
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-03-26 00:09:16 -0300

    Day4 - adding extend to this exercise

--- day4/main1.py
+++ day4/main1.py
@@ -4,6 +4,8 @@ estados_do_brasil[-2] = "Goiaba"
 
 estados_do_brasil.append("Tocantinho")
 
+estados_do_brasil.extend(["Fernando de Noronha", "Distrito Federal"])
+
 print(estados_do_brasil)
 
 # print(estados_do_brasil[0], estados_do_brasil[-1], estados_do_brasil[-2])
