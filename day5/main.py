c18487c5387ebe2771197be67a7b25c0554d4c2b
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-03-28 09:54:48 -0300

    Day5 - introducing loops

---
+++ day5/main.py
@@ -0,0 +1,7 @@
+estados_do_brasil = ["Minas Gerais", "Goiás", "Rio Grande do Sul", "Rio Grande do Norte", "Roraima", "Rio de Janeiro", "Mato Grosso do Sul", "Amazônia", "Espírito Santo", "Bahia", "Pernambuco", "Maranhão", "Acre", "Sergipe", "Paraná"]
+count = 0
+print(len(estados_do_brasil))
+for estado in estados_do_brasil:
+    print(estado)
+    count += 1
+    print(count)
