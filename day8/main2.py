9860b19ff334b0dee1b8a31004aeede544b10157
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-09-28 18:59:01 -0300

    Create main2.py

---
+++ day8/main2.py
@@ -0,0 +1,52 @@
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+
+def caesar(drt, txt, shift):
+
+    if drt == "decode":
+        alphabet.reverse()
+        shift *= -1
+
+    def encrypt_or_decrypt(tx, sft):
+        new_text = ""
+        for letter in tx:
+            letter_index = alphabet.index(letter)
+            letter_new_index = letter_index + sft
+            new_text += alphabet[letter_new_index % len(alphabet)]
+        print(new_text)
+
+    encrypt_or_decrypt(tx=txt, sft=shift)
+
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift_input = int(input("Type the shift number:\n"))
+
+caesar(drt=direction, txt=text, shift=shift_input)
