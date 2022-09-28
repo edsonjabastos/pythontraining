b33761289962470e801cbda8809e89bc2aef6438
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-09-28 18:58:58 -0300

    Create main1.py

---
+++ day8/main1.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+
+def prime_checker(number):
+    is_prime = (int(number /number)) == 1
+    print(is_prime)
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
