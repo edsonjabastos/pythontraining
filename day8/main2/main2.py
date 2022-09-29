c6b9a2f199cd98e77195bf9ee2fe7137e04d46ef
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-09-29 16:47:06 -0300

    Update main2.py

--- day8/main2/main2.py
+++ day8/main2/main2.py
@@ -27,35 +27,53 @@ alphabet = [
     "z",
 ]
 from art import *
+
 print(logo)
+
 
 def caesar(drt, txt, shift):
 
-    print(drt)
-    print(shift)
+    while drt != "encode" and drt != "decode":
+        drt = input("Accept only 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    # while alphabet.count(txt) > 0:
+    #     txt = input("Accept only letters of alphabet.\n")
+
+    # print(drt)
+    # print(shift)
     if drt == "decode":
-        # alphabet.reverse()
         shift *= -1
-    print(shift)
-    print(txt)
-    print(alphabet)
+    # print(shift)
+    # print(txt)
+    # print(alphabet)
 
     new_text = ""
     for letter in txt:
         letter_index = alphabet.index(letter)
         letter_new_index = letter_index + shift
         new_text += alphabet[letter_new_index % len(alphabet)]
-        print(new_text)
-        print(letter)
-        print(shift)
-        print(letter_index)
-        print(alphabet[letter_new_index % len(alphabet)])
+        # print(new_text)
+        # print(letter)
+        # print(shift)
+        # print(letter_index)
+        # print(alphabet[letter_new_index % len(alphabet)])
     print(f"The {drt}d text is {new_text}.")
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift_input = int(input("Type the shift number:\n"))
 
-caesar(drt=direction, txt=text, shift=shift_input)
+game_is_on = True
+
+while game_is_on:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift_input = int(input("Type the shift number:\n"))
+
+    caesar(drt=direction, txt=text, shift=shift_input)
+
+    want_to_leave = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if want_to_leave == "no":
+        game_is_on = False
+
+print("Bye! Finishing > Caesar Cipher < ...")
 import os
-os.system('pause')
+
+os.system("pause")
