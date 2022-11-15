922fbb81a774b8b16d016a5c8300ef884fc9c80c
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-15 19:01:32 -0300

    Update main.py

--- day10/main.py
+++ day10/main.py
@@ -1,7 +1,10 @@
 # Functions with outputs
+string_length = len("String")
+# Docstring comments
 
 
 def format_name(f_name, l_name):
+    """Returns first name and last name capitalized."""
     if f_name == "" or l_name == "":
         return "You didn't provide valid inputs"
     return f"Result: {f_name.title()} {l_name.title()}"
