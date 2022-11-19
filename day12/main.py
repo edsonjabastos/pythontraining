61fb79579769f40700a35aa86d6257d97b207579
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-18 23:59:33 -0300

    Update main.py

--- day12/main.py
+++ day12/main.py
@@ -1,5 +1,7 @@
 import random
+from art import logo
 
+print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100")
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
