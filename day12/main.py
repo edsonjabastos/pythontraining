ded8207150f05e1fbcdfbcc6e030ff787a075ca3
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-18 23:52:17 -0300

    Create main.py

---
+++ day12/main.py
@@ -0,0 +1,29 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+attempts = 0
+if difficulty == "easy":
+    attempts = 10
+elif difficulty == "hard":
+    attempts = 5
+random_number = random.randint(0, 100)
+while attempts > 0:
+    print(f"You have {attempts} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+    if guess == random_number:
+        print(f"You got it! The answer was {random_number}")
+        attempts = 0
+    elif guess > random_number:
+        print("Too high.")
+        attempts -= 1
+        if attempts == 0:
+            print("You've run out of guesses, you lose.")
+        print("Guess again.")
+    elif guess < random_number:
+        print("Too low.")
+        attempts -= 1
+        if attempts == 0:
+            print("You've run out of guesses, you lose.")
+        print("Guess again.")
