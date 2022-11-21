d4e75cbd2824ed20e08b47222530124ecf5adea7
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-21 08:26:20 -0300

    Create main.py

---
+++ day14/main.py
@@ -0,0 +1,52 @@
+from art import logo, vs
+from data import data
+from random import randint
+import os
+
+print(logo)
+
+
+def random_pick(data_array):
+    random_choice = randint(0, len(data) - 1)
+    return data_array[random_choice]
+
+
+score = 0
+
+player_lost = False
+a_pick = random_pick(data)
+while not player_lost:
+    b_pick = random_pick(data)
+    print(
+        f"Compare A: {a_pick['name']}, a {a_pick['description']}, from  {a_pick['country']}"
+    )
+    print(vs)
+    print(
+        f"Against B: {b_pick['name']}, a {b_pick['description']}, from  {b_pick['country']}"
+    )
+    guess = input("Who has more followers?: Type 'A' or 'B': ").upper()
+    print(guess)
+    if guess == "A":
+        if a_pick["follower_count"] > b_pick["follower_count"]:
+            score += 1
+            os.system("cls")
+            print(logo)
+            print(f"You're right! Current score: {score}.")
+            a_pick = b_pick
+        else:
+            player_lost = True
+            os.system("cls")
+            print(logo)
+            print(f"Sorry, that's wrong. Final Score: {score}")
+
+    elif guess == "B":
+        if b_pick["follower_count"] > a_pick["follower_count"]:
+            score += 1
+            os.system("cls")
+            print(logo)
+            print(f"You're right! Current score: {score}.")
+        else:
+            player_lost = True
+            os.system("cls")
+            print(logo)
+            print(f"Sorry, that's wrong. Final Score: {score}")
