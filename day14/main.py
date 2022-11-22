74745e1320a4b346807a6a73bc1170c9e3108886
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-22 20:40:32 -0300

    Update main.py

--- day14/main.py
+++ day14/main.py
@@ -11,42 +11,55 @@ def random_pick(data_array):
     return data_array[random_choice]
 
 
-score = 0
+def game():
+    score = 0
+    player_lost = False
+    a_pick = random_pick(data)
+    last_pick = 0
+    while not player_lost:
+        b_pick = random_pick(data)
 
-player_lost = False
-a_pick = random_pick(data)
-while not player_lost:
-    b_pick = random_pick(data)
-    print(
-        f"Compare A: {a_pick['name']}, a {a_pick['description']}, from  {a_pick['country']}"
-    )
-    print(vs)
-    print(
-        f"Against B: {b_pick['name']}, a {b_pick['description']}, from  {b_pick['country']}"
-    )
-    guess = input("Who has more followers?: Type 'A' or 'B': ").upper()
-    print(guess)
-    if guess == "A":
-        if a_pick["follower_count"] > b_pick["follower_count"]:
-            score += 1
-            os.system("cls")
-            print(logo)
-            print(f"You're right! Current score: {score}.")
-            a_pick = b_pick
-        else:
-            player_lost = True
-            os.system("cls")
-            print(logo)
-            print(f"Sorry, that's wrong. Final Score: {score}")
+        while a_pick == b_pick:
+            b_pick = random_pick(data)
 
-    elif guess == "B":
-        if b_pick["follower_count"] > a_pick["follower_count"]:
-            score += 1
-            os.system("cls")
-            print(logo)
-            print(f"You're right! Current score: {score}.")
+        print(
+            f"Compare A: {a_pick['name']}, a {a_pick['description']}, from  {a_pick['country']}"
+        )
+        print(vs)
+        print(
+            f"Against B: {b_pick['name']}, a {b_pick['description']}, from  {b_pick['country']}"
+        )
+        guess = input("Who has more followers?: Type 'A' or 'B': ").upper()
+
+        if guess == "A":
+            if a_pick["follower_count"] > b_pick["follower_count"]:
+                score += 1
+                os.system("cls")
+                print(logo)
+                print(f"You're right! Current score: {score}.")
+                a_pick = b_pick
+            else:
+                player_lost = True
+                os.system("cls")
+                print(logo)
+                print(f"Sorry, that's wrong. Final Score: {score}")
+
+        elif guess == "B":
+            if b_pick["follower_count"] > a_pick["follower_count"]:
+                score += 1
+                os.system("cls")
+                print(logo)
+                print(f"You're right! Current score: {score}.")
+                a_pick = b_pick
+
+            else:
+                player_lost = True
+                os.system("cls")
+                print(logo)
+                print(f"Sorry, that's wrong. Final Score: {score}")
         else:
-            player_lost = True
-            os.system("cls")
-            print(logo)
-            print(f"Sorry, that's wrong. Final Score: {score}")
+            return
+
+
+game()
+os.system("pause")
