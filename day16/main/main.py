03ad625d6f990ecdcc743c2b0f5f7d0e8ff1d783
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-25 08:23:21 -0300

    Update main.py

--- day16/main/main.py
+++ day16/main/main.py
@@ -1,3 +1,23 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee = CoffeeMaker()
+money = MoneyMachine()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}):")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink is not None:
+            if coffee.is_resource_sufficient(drink) and money.make_payment(drink.cost):
+                coffee.make_coffee(drink)
