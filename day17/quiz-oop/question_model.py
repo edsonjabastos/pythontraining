9a085a94342b61fac2edca078857cb01066f6de6
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-30 07:32:20 -0300

    Update question_model.py

--- day17/quiz-oop/question_model.py
+++ day17/quiz-oop/question_model.py
@@ -1,8 +1,18 @@
 class Question:
-    def __init__(self, q_text, q_answer):
+    def __init__(self,
+                 q_text,
+                 q_answer,
+                 q_category,
+                 q_type,
+                 q_difficulty,
+                 q_incorrect_answers
+                 ):
         print("Creating question...")
         self.text = q_text
         self.answer = q_answer
-
+        self.category = q_category
+        self.type = q_type
+        self.difficulty = q_difficulty
+        self.incorrect_answers = q_incorrect_answers
 
 # q1 = Question('2+3=5', 'True')
