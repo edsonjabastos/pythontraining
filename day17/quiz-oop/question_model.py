fc750426fffe10bca291ea2a7011c52aba2158b6
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-11-29 22:25:51 -0300

    Create question_model.py

---
+++ day17/quiz-oop/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self, q_text, q_answer):
+        print("Creating question...")
+        self.text = q_text
+        self.answer = q_answer
+
+
+# q1 = Question('2+3=5', 'True')
