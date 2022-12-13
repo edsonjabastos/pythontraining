f4df2ea0eca3a05b2ff481a9116a1b1ae1eb076a
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2022-12-13 10:36:58 -0300

    Create main.py

---
+++ day18/main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+michael_the_big = Turtle()
+
+michael_the_big.shape('turtle')
+
+michael_the_big.circle(100, 100, 100)
+
+michael_the_big.left(90)
+michael_the_big.forward(100)
+michael_the_big.left(90)
+michael_the_big.forward(100)
+michael_the_big.left(90)
+michael_the_big.forward(100)
+michael_the_big.left(90)
+michael_the_big.forward(100)
+
+screen = Screen()
+screen.exitonclick()
