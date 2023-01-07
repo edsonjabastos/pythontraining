36ef0b5519c7b6146d2a016908b89ff22ed552c4
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-06 23:24:16 -0300

    Update main.py

--- day18/main.py
+++ day18/main.py
@@ -6,6 +6,7 @@ import turtle as t
 
 michael_the_big = t.Turtle()
 michael_the_big.shape("turtle")
+michael_the_big.speed("fastest")
 
 # michael_the_big.circle(100, 100, 100)
 #
@@ -62,44 +63,53 @@ michael_the_big.shape("turtle")
 #         michael_the_big.left(180)
 #         michael_the_big.forward(10)
 # colors = ["red", "green", "blue", "orange", "purple", "brown", "yellow"]
-# t.colormode(255)
+t.colormode(255)
 
+screen = t.Screen()
+screen.bgcolor("black")
 
 def random_color():
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
-    return (r, g, b)
+    color = (r, g, b)
+    return color
     # return f"rgb({r},{g},{b})"
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        michael_the_big.color(random_color())
+        michael_the_big.circle(100)
+        michael_the_big.setheading(michael_the_big.heading() + size_of_gap)
 
-screen = t.Screen()
-screen.bgcolor("black")
+draw_spirograph(2)
 
 # colorRandomized = random_color()
 # print(colorRandomized)
 
 # directions = [0, 90, 180, 270]
 # michael_the_big.pensize(15)
-michael_the_big.speed("fastest")
 
 # for _ in range(200):
 #     michael_the_big.color(random_color())
 #     michael_the_big.forward(30)
 #     michael_the_big.setheading(choice(directions))
 
-for i in range(6):
-   
-      # Choose your color combination
-    for color in ('red', 'magenta', 'blue',
-                  'cyan', 'green', 'white',
-                  'yellow'):
-        michael_the_big.color(color)
-         
-        # Draw a circle of chosen size, 100 here
-        michael_the_big.circle(100)
-         
-        # Move 10 pixels left to draw another circle
-        michael_the_big.left(10)
+
+
+# for i in range(6):
+
+#       # Choose your color combination
+#     for color in ('red', 'magenta', 'blue',
+#                   'cyan', 'green', 'white',
+#                   'yellow'):
+#         michael_the_big.color(color)
+
+#         # Draw a circle of chosen size, 100 here
+#         michael_the_big.circle(100)
+
+#         # Move 10 pixels left to draw another circle
+#         michael_the_big.left(10)
+
 
 screen.exitonclick()
 # import heroes
