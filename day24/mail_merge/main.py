0195d1818bcf3871dd65a5c2caf0283abc97cbd8
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 19:57:52 -0300

    Update main.py

--- day24/mail_merge/main.py
+++ day24/mail_merge/main.py
@@ -1,26 +1,26 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 import os
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 PLACE_HOLDER = "[name]"
 
 with open("Input/Names/invited_names.txt") as f1:
     content1 = f1.readlines()
-    for name in content1:
-        name = name.strip()
-        with open("Input/Letters/starting_letter.txt") as f2:
-            # print(name)
-            content2 = f2.read()
-            # print(content2)
-            # print(content1)
-            with open(f"Output/ReadyToSend/{name}_letter",'w') as f3:
-                f3.write(content2.replace(PLACE_HOLDER, f"{name}"))
-                
+with open("Input/Letters/starting_letter.txt") as f2:
+    # print(name)
+    content2 = f2.read()
+    # print(content2)
+    # print(content1)
+for name in content1:
+    name = name.strip()
+    with open(f"Output/ReadyToSend/{name}_letter", "w") as f3:
+        f3.write(content2.replace(PLACE_HOLDER, f"{name}"))
