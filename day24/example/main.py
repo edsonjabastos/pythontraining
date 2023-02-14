13af21d19b92e51c532ab0f7d0602980c94578c7
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 19:21:23 -0300

    Update main.py

--- day24/example/main.py
+++ day24/example/main.py
@@ -3,8 +3,11 @@ print("File      Path:", Path(__file__).absolute())
 print("Directory Path:", Path().absolute()) # Directory of current working directory, not __file__ 
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-with open("..\\a.txt") as file:
+with open("../a.txt") as file:
     contents = file.read()
     print(contents)
 print("File      Path:", Path(__file__).absolute())
 print("Directory Path:", Path().absolute()) # Directory of current working directory, not __file__ 
+with open("../../.gitignore") as file:
+    contents = file.read()
+    print(contents)
