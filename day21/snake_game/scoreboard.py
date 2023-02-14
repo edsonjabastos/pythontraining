6c5e6a0813e203ad278d98c3235b5e9cfc0aac97
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 17:23:33 -0300

    Update scoreboard.py

--- day21/snake_game/scoreboard.py
+++ day21/snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color("white")
         self.goto(0, 260)
@@ -15,14 +16,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(0, 260)
-        self.write(f"Score = {self.score}", True, align=ALIGNMENT, font=FONT)
+        self.write(f"Score = {self.score} High Score = {self.high_score}", True, align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER.", True, align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
