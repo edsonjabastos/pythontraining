9bbdace0e94c9f943c7bdc08a9482fe245d041b0
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 20:39:28 -0300

    Update main.py

--- day25/main.py
+++ day25/main.py
@@ -23,6 +23,18 @@ import pandas
 
 data = pandas.read_csv("weather_data.csv")
 
-print(data)
-print(data["temp"])
-print(data.temp)
+# print(data)
+# print(data["temp"])
+# print(data.temp)
+
+data_dict = data.to_dict()
+print(data_dict)
+print(data.temp.to_list())
+
+avg = sum(data.temp.to_list()) / len(data.temp.to_list())
+print(avg)
+
+print(data.temp.mean())
+print(data.temp.max())
+
+print(data.condition)
