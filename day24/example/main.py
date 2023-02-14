10efd5da777f18ea2d08cc6ebc111e8aa8038ee0
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 19:07:42 -0300

    Create main.py

---
+++ day24/example/main.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+print("File      Path:", Path(__file__).absolute())
+print("Directory Path:", Path().absolute()) # Directory of current working directory, not __file__ 
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+with open("..\\a.txt") as file:
+    contents = file.read()
+    print(contents)
+print("File      Path:", Path(__file__).absolute())
+print("Directory Path:", Path().absolute()) # Directory of current working directory, not __file__ 
