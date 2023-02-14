69dc83739cf7dfaac3e9cf52720dfba577389659
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 20:03:43 -0300

    Update main.py

--- day24/mail_merge/main.py
+++ day24/mail_merge/main.py
@@ -13,14 +13,13 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 PLACE_HOLDER = "[name]"
 
-with open("Input/Names/invited_names.txt") as f1:
-    content1 = f1.readlines()
-with open("Input/Letters/starting_letter.txt") as f2:
-    # print(name)
-    content2 = f2.read()
-    # print(content2)
-    # print(content1)
-for name in content1:
+with open("Input/Names/invited_names.txt") as names_file:
+    names = names_file.readlines()
+with open("Input/Letters/starting_letter.txt") as letter_txt_file:
+
+    letter_txt = letter_txt_file.read()
+
+for name in names:
     name = name.strip()
     with open(f"Output/ReadyToSend/{name}_letter", "w") as f3:
-        f3.write(content2.replace(PLACE_HOLDER, f"{name}"))
+        f3.write(letter_txt.replace(PLACE_HOLDER, f"{name}"))
