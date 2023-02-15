9e0df3c32e3f13c7af3041da9bf475a3ff8dcd07
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-14 22:20:45 -0300

    Update main.py

--- day25/main.py
+++ day25/main.py
@@ -24,8 +24,10 @@ import pandas
 data = pandas.read_csv("weather_data.csv")
 
 # print(data)
-# print(data["temp"])
-# print(data.temp)
+print(data["temp"])
+print(data.temp)
+# print(data["Temp"])
+# print(data.Temp)
 
 data_dict = data.to_dict()
 print(data_dict)
@@ -38,3 +40,10 @@ print(data.temp.mean())
 print(data.temp.max())
 
 print(data.condition)
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+monday = data[data.day == "Monday"]
+print(monday.condition)
+
+for temp in data.temp.to_list():
+    print(f"{(temp*(9/5)+32)}°F")
