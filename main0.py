26371cf22c5eaee8a9050965f27b20737e0268bd
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-16 15:55:52 -0300

    Create main0.py

---
+++ main0.py
@@ -0,0 +1,20 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+
+# Write your code below:
+
+words_and_lengths = {word: len(word) for word in sentence.split(" ")}
+
+print(words_and_lengths)
+
+{
+    "What": 4,
+    "is": 2,
+    "the": 3,
+    "Airspeed": 8,
+    "Velocity": 8,
+    "of": 2,
+    "an": 2,
+    "Unladen": 7,
+    "Swallow?": 8,
+}
