904830d3f5cd98174f52e2f71d1ab852c921b461
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-02-16 14:57:55 -0300

    Create main.py

---
+++ day26/main.py
@@ -0,0 +1,33 @@
+# numbers = [1,2,3]
+# new_numbers = [ n + 1 for n in numbers]
+# name = "Angela"
+# name_list = [ letter for letter in name ]
+# print(new_numbers)
+# print(name_list)
+# lista = []
+# # for i in range(1,5):
+# #     lista.append(i)
+# dd = [ n * 2 for n in range(1,5)]
+
+# print(dd)
+
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+# short_name = [name for name in names if len(name) < 5]
+# large_names_in_caps = [name.upper() for name in names if len(name) > 5]
+
+
+# squared = [n * n for n in range(10)]
+
+# print(squared)
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# # 🚨 Do Not Change the code above 👆
+
+# #Write your 1 line code 👇 below:
+
+# squared_numbers  = [n * n for n in numbers]
+
+# #Write your code 👆 above:
+
+# print(squared_numbers)
